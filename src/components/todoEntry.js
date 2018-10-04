4d7164d80d570fc0f61947cb92bfd46e97d16edd
Willdta <beyondutraining@gmail.com>
2018-10-04 10:50:33 -0400

    updated addtodo function to take in both text and tags

--- src/components/todoEntry.js
+++ src/components/todoEntry.js
@@ -6,7 +6,7 @@ import {observer} from 'mobx-react';
 const ENTER_KEY = 13;
 
 @observer export default class TodoEntry extends React.Component {
-		addTags = () => {
+	addTags = () => {
 		const { addTags } = this.props.todoStore
 		const { value } = this.refs.tagValue
 	
@@ -16,57 +16,53 @@ const ENTER_KEY = 13;
 		}
 	}
 
+	addTodo = () => {
+		const { addTodo, tagDescriptions } = this.props.todoStore
+		const { value } = this.refs.newField
+		
+		if (value && tagDescriptions.length > 0) {
+			addTodo(value, tagDescriptions)
+			this.refs.newField.value = ''
+		}
+	}
+
 	render() {
 		const { tagDescriptions } = this.props.todoStore
 
 		return (
-		<div>
-			<input
-				ref="newField"
-				className="new-todo"
-				placeholder="What needs to be done?"
-				onKeyDown={this.handleNewTodoKeyDown}
-				autoFocus={true}
-			/>
-			<input
-				ref="tagValue"
-				className="new-todo"
-				placeholder="Add a tag"
-				autoFocus={true}
-				onKeyPress={e => {
-					e.key === 'Enter' && (
-						this.addTags()
-					)
-				}}
-			/>
+			<div className="input-section">
+				<input
+					ref="newField"
+					className="new-todo"
+					placeholder="What needs to be done?"
+					autoFocus={true}
+				/>
+				<input
+					ref="tagValue"
+					className="new-todo"
+					placeholder="Add a tag"
+					autoFocus={true}
+					onKeyPress={e => {
+						e.key === 'Enter' && (
+							this.addTags()
+						)
+					}}
+				/>
 
-			{tagDescriptions.map((tag, index) => (
-				<span key={index}>{tag}</span>)
-			)}
+				{tagDescriptions.map((tag, index) => (
+					<span key={index}>{tag}</span>)
+				)}
 
-			<div className="add-todo-button-container">
-				<button className="button-style">
-					Add Todo
-				</button>
+				<div className="add-todo-button-container">
+					<button 
+						className="button-style"
+						onClick={() => this.addTodo()}>
+						Add Todo
+					</button>
+				</div>
 			</div>
-		</div>
 		);
 	}
-
-	handleNewTodoKeyDown = (event) => {
-		if (event.keyCode !== ENTER_KEY) {
-			return;
-		}
-
-		event.preventDefault();
-
-		var val = ReactDOM.findDOMNode(this.refs.newField).value.trim();
-
-		if (val) {
-			this.props.todoStore.addTodo(val);
-			ReactDOM.findDOMNode(this.refs.newField).value = '';
-		}
-	};
 }
 
 TodoEntry.propTypes = {
