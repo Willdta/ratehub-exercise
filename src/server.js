e7e0d158cfe89b10fbe416e03963c5e5e30fd7a2
Willdta <beyondutraining@gmail.com>
2018-10-05 11:12:08 -0400

    added additional instructions/added cursor pointer to multiple elements

--- src/server.js
+++ src/server.js
@@ -42,6 +42,8 @@ const renderFullPage = html => {
 			<script src="/static/bundle.js"></script>
 			<footer class="info">
 				<p>Double-click to edit a todo</p>
+				<p>Enter a tag by clicking enter</p>
+				<p>Filter by buttons or tags that you created</p>
 				<p>TodoMVC powered by React and <a href="http://github.com/mobxjs/mobx/">MobX</a>. Created by <a href="http://github.com/mweststrate/">mweststrate</a></p>
 				<p>Based on the base React TodoMVC by <a href="http://github.com/petehunt/">petehunt</a></p>
 				<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
